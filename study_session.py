e1d3b3551c448b08a699ee4d74b6ecb7588d8509
Alina <apaiz@xula.edu>
2025-11-24 20:50:58 -0600

    create invite method

--- study_session.py
+++ study_session.py
@@ -1,6 +1,7 @@
 #Implement StudySession Class
 #Attributes: proposer, time, place, topic, status
 #Methods: invite(), confirm(), cancel()
+
 class StudySession:
     def __init__(self, proposer, time, place, topic, status="pending"):
         self.proposer = proposer
@@ -9,8 +10,15 @@ class StudySession:
         self.topic = topic
         self.status = status
 
-    def invite(self, invitee):
-        print(f"Inviting {invitee} to study session on {self.topic}")
+    def invite(self, profile1, profile2):
+        times_in_profile1 = any(session.time == self.time for session in profile1.schedule)
+        times_in_profile2 = any(session.time == self.time for session in profile2.schedule)
+        if not times_in_profile1 and not times_in_profile2:
+            profile1.update_schedule(self)
+            profile2.update_schedule(self)
+            print(f"Invited {profile1.first_name} and {profile2.first_name} to study session on {self.topic} at {self.time}")
+        else:
+            print("Cannot invite: Time conflict in one or both profiles' schedules.")
 
     def confirm(self):
         print(f"Confirming study session on {self.topic}")
